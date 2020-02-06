ba109837e019a12cb07abab675a3b56f4f257d46
Gabriel Acosta Belchior <gabriel.acostab@hotmail.com>
2020-02-06 19:18:01 -0300

    Basic architecture

---
+++ CRUDContacts.Interfaces/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CRUDContacts.Interfaces
+{
+    public class Class1
+    {
+    }
+}
---
+++ CRUDContacts.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CRUDContacts.Models
+{
+    public class Class1
+    {
+    }
+}
